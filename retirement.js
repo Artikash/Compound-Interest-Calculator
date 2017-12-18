7f0c9bd5a3fad3d5c5b9b8dd3fad484f9de7d87c
Akash Mozumdar <akashmozumdar@gmail.com>
2017-12-18 15:29:39 -0500

    bugfix

--- retirement.js
+++ retirement.js
@@ -14,20 +14,22 @@ function calculateTimeToZero() {
   var retirementLengthDisplay = document.createElement("p");
   retirementLengthDisplay.textContent =
     "Retirement will last: " +
-    (principalByYear.length > 5000 ? "Indefinitely." : principalByYear.length - parseInt(document.getElementById("years").value, 10) + " years.");
+    (principalByYear.length > 5000 ? "Indefinitely." : principalByYear.length - parseInt(document.getElementById("years").value, 10) - 1 + " years.");
   output.appendChild(retirementLengthDisplay);
   var incomeAtRetirement = document.createElement("p");
   incomeAtRetirement.textContent =
     "Income at start of retirement: $" +
-    parseFloat(document.getElementById("interestRate").value) *
-      0.01 *
-      principalByYear[parseFloat(document.getElementById("years").value)] /
-      Math.pow(1.03, parseFloat(document.getElementById("years").value));
+    Math.floor(
+      parseFloat(document.getElementById("interestRate").value) *
+        0.01 *
+        principalByYear[parseFloat(document.getElementById("years").value)] /
+        Math.pow(1.03, parseFloat(document.getElementById("years").value))
+    );
   output.appendChild(incomeAtRetirement);
   if (principalByYear.length < 5000) {
     for (var j = 0; j < principalByYear.length; j++) {
       var display = document.createElement("p");
-      display.textContent = "Year " + j + ": " + Math.floor(principalByYear[j] / Math.pow(1.03, j));
+      display.textContent = "Year " + j + ": $" + Math.floor(principalByYear[j] / Math.pow(1.03, j));
       output.appendChild(display);
     }
   }
