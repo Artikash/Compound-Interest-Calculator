ec16158b99c2a30e7d9d5a38d9892bd9f1664856
Akash Mozumdar <akashmozumdar@gmail.com>
2017-12-18 15:53:10 -0500

    bugfixes

--- retirement.js
+++ retirement.js
@@ -1,30 +1,22 @@
-function NewCompoundingInterest() {
-  return new CompoundingInterest(parseFloat(document.getElementById("principal").value), parseFloat(document.getElementById("interestRate").value) / 100);
-}
-
 function calculateTimeToZero() {
   var output = document.getElementById("output");
   while (output.firstChild) {
     output.removeChild(output.firstChild);
   }
-  var yearlyContributions = Array(parseInt(document.getElementById("years").value, 10)).fill(parseFloat(document.getElementById("yearlyAddition").value));
+  var principal = parseFloat(document.getElementById("principal").value);
+  var interestRate = parseFloat(document.getElementById("interestRate").value) / 100;
+  var years = parseInt(document.getElementById("years").value, 10);
+  var inflation = 1 + parseFloat(document.getElementById("inflationRate").value) / 100;
+  var yearlyContributions = Array(years).fill(parseFloat(document.getElementById("yearlyAddition").value));
   yearlyContributions = yearlyContributions.concat(Array(100).fill(-document.getElementById("yearlyWithdrawal").value));
-  for (var i = 0; i < yearlyContributions.length; i++) yearlyContributions[i] *= Math.pow(1.03, i);
-  var principalByYear = NewCompoundingInterest().simulateUntilZero(yearlyContributions);
+  for (var i = 0; i < yearlyContributions.length; i++) yearlyContributions[i] *= Math.pow(inflation, i);
+  var principalByYear = new CompoundingInterest(principal, interestRate).simulateUntilZero(yearlyContributions);
   var retirementLengthDisplay = document.createElement("p");
   retirementLengthDisplay.textContent =
-    "Retirement will last: " +
-    (principalByYear.length > 5000 ? "Indefinitely." : principalByYear.length - parseInt(document.getElementById("years").value, 10) - 1 + " years.");
+    "Retirement will last: " + (principalByYear.length > 5000 ? "Indefinitely." : principalByYear.length - years - 1 + " years.");
   output.appendChild(retirementLengthDisplay);
   var incomeAtRetirement = document.createElement("p");
-  incomeAtRetirement.textContent =
-    "Income at start of retirement: $" +
-    Math.floor(
-      parseFloat(document.getElementById("interestRate").value) *
-        0.01 *
-        principalByYear[parseFloat(document.getElementById("years").value)] /
-        Math.pow(1.03, parseFloat(document.getElementById("years").value))
-    );
+  incomeAtRetirement.textContent = "Income at start of retirement: $" + Math.floor(interestRate * principalByYear[years] / Math.pow(inflation, years));
   output.appendChild(incomeAtRetirement);
   if (principalByYear.length < 5000) {
     for (var j = 0; j < principalByYear.length; j++) {
