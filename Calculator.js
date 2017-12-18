a6995f21ee062d70ae5b51ea45839dcf3beddfaa
Akash Mozumdar <akashmozumdar@gmail.com>
2017-12-18 15:16:59 -0500

    change generic calculator into retirement

--- Calculator.js
+++ Calculator.js
@@ -1,33 +1,36 @@
 window.CompoundingInterest = function(principal, interestRate) {
   this.principal = principal;
   this.interestRate = interestRate;
-  this.finalAmount = function(time, monthlyAddition) {
+
+  this.finalAmount = function(time, yearlyAddition) {
     var currentPrincipal = this.principal;
     for (var i = 0; i < time; i++) {
-      currentPrincipal += monthlyAddition;
+      currentPrincipal += yearlyAddition[i] || yearlyAddition[yearlyAddition.length - 1];
       currentPrincipal *= 1 + this.interestRate;
     }
     return currentPrincipal;
   };
-  this.timeToZero = function(monthlyAddition) {
-    var currentPrincipal = this.principal;
-    var time = 0;
-    while (currentPrincipal >= 0) {
-      time++;
-      currentPrincipal += monthlyAddition;
+
+  this.simulateUntilZero = function(yearlyAddition) {
+    var principalByYear = [this.principal];
+    while (principalByYear[principalByYear.length - 1] >= 0) {
+      var currentPrincipal = principalByYear[principalByYear.length - 1];
+      currentPrincipal += yearlyAddition[principalByYear.length - 1] || yearlyAddition[yearlyAddition.length - 1];
       currentPrincipal *= 1 + this.interestRate;
-      if (currentPrincipal >= this.principal) {
-        return Infinity;
+      principalByYear.push(currentPrincipal);
+      if (principalByYear.length > 5000) {
+        break;
       }
     }
-    return time;
+    return principalByYear;
   };
+
   this.monthlyPayments = function(time) {
-    var testPayment = this.finalAmount(time, 0) / time;
-    var testAmount = this.finalAmount(time, testPayment);
+    var testPayment = this.finalAmount(time, [0]) / time;
+    var testAmount = this.finalAmount(time, [testPayment]);
     while (Math.abs(testAmount) < 10) {
       testPayment = testAmount > 0 ? testPayment * 1.1 : testPayment * 0.9;
-      testAmount = this.finalAmount(time, testPayment);
+      testAmount = this.finalAmount(time, [testPayment]);
     }
     return testPayment;
   };
---
+++ retirement.js
@@ -0,0 +1,34 @@
+function NewCompoundingInterest() {
+  return new CompoundingInterest(parseFloat(document.getElementById("principal").value), parseFloat(document.getElementById("interestRate").value) / 100);
+}
+
+function calculateTimeToZero() {
+  var output = document.getElementById("output");
+  while (output.firstChild) {
+    output.removeChild(output.firstChild);
+  }
+  var yearlyContributions = Array(parseInt(document.getElementById("years").value, 10)).fill(parseFloat(document.getElementById("yearlyAddition").value));
+  yearlyContributions = yearlyContributions.concat(Array(100).fill(-document.getElementById("yearlyWithdrawal").value));
+  for (var i = 0; i < yearlyContributions.length; i++) yearlyContributions[i] *= Math.pow(1.03, i);
+  var principalByYear = NewCompoundingInterest().simulateUntilZero(yearlyContributions);
+  var retirementLengthDisplay = document.createElement("p");
+  retirementLengthDisplay.textContent =
+    "Retirement will last: " +
+    (principalByYear.length > 5000 ? "Indefinitely." : principalByYear.length - parseInt(document.getElementById("years").value, 10) + " years.");
+  output.appendChild(retirementLengthDisplay);
+  var incomeAtRetirement = document.createElement("p");
+  incomeAtRetirement.textContent =
+    "Income at start of retirement: $" +
+    parseFloat(document.getElementById("interestRate").value) *
+      0.01 *
+      principalByYear[parseFloat(document.getElementById("years").value)] /
+      Math.pow(1.03, parseFloat(document.getElementById("years").value));
+  output.appendChild(incomeAtRetirement);
+  if (principalByYear.length < 5000) {
+    for (var j = 0; j < principalByYear.length; j++) {
+      var display = document.createElement("p");
+      display.textContent = "Year " + j + ": " + Math.floor(principalByYear[j] / Math.pow(1.03, j));
+      output.appendChild(display);
+    }
+  }
+}
